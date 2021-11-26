6347817c3fd15cc66752d56878919ac280d0e09c
Kacper13b <kacper.banasik13b@gmail.com>
2021-11-26 15:26:58 +0100

    deployment .......

--- SQLProject/settings.py
+++ SQLProject/settings.py
@@ -23,7 +23,7 @@ PROJECT_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '+4=2b(z21*tjpgiezf@mp*=jvtcrj+q4kw*h@2#3my7$7(03@5'
+SECRET_KEY = '*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
