48630b1c689dd52a0b7b4a4ef8aff1c431478b32
Kacper13b <kacper.banasik13b@gmail.com>
2021-12-01 23:49:55 +0100

    changes

--- SQLProject/settings.py
+++ SQLProject/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 from pathlib import Path
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = Path(__file__).resolve().parent.parent
